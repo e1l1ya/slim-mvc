86c28aae97470856a31885f05b23aa425a7e5263
eiliya keshtkar <e1l1yasec@gmail.com>
2023-10-13 11:43:34 +0330

    Cache Remove

--- storage/cache/61174ac665477fc219b11378672fc72fdf9090eb.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <div style="padding: 10px">
-        <p>
-            Not found
-        </p>
-    </div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\mysite.example\lab.mysite.example\resources\views/error/notfound.blade.php ENDPATH**/ ?>
